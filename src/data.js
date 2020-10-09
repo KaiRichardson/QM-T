4cd0b4704b66e9c5789d23afeff558d7bcc9b55e
Kai Richardson <kairichardson2000@gmail.com>
2020-10-09 11:52:55 -0700

    adding all

---
+++ src/data.js
@@ -0,0 +1,282 @@
+export default {
+  posts: [
+    {
+      id: 1,
+      ticket:
+        "Accusantium qui nihil voluptatum quia voluptas maxime ab similique",
+      note:
+        "<p>Rerum velit quos est <strong>similique</strong>. Consectetur tempora eos ullam velit nobis sit debitis. Magni explicabo omnis delectus labore vel recusandae.</p><p>Aut a minus laboriosam harum placeat quas minima fuga. Quos nulla fuga quam officia tempore. Rerum occaecati ut eum et tempore. Nam ab repudiandae et nemo praesentium.</p><p>Cumque corporis officia occaecati ducimus sequi laborum omnis ut. Nam aspernatur veniam fugit. Nihil eum libero ea dolorum ducimus impedit sed. Quidem inventore porro corporis debitis eum in. Nesciunt unde est est qui nulla. Esse sunt placeat molestiae molestiae sed quia. Sunt qui quidem quos velit reprehenderit quos blanditiis ducimus. Sint et molestiae maxime ut consequatur minima. Quaerat rem voluptates voluptatem quos. Corporis perferendis in provident iure. Commodi odit exercitationem excepturi et deserunt qui.</p><p>Optio iste necessitatibus velit non. Neque sed occaecati culpa porro culpa. Quia quam in molestias ratione et necessitatibus consequatur. Est est tempora consequatur voluptatem vel. Mollitia tenetur non quis omnis perspiciatis deserunt sed necessitatibus. Ad rerum reiciendis sunt aspernatur.</p><p>Est ullam ut magni aspernatur. Eum et sed tempore modi.</p><p>Earum aperiam sit neque quo laborum suscipit unde. Expedita nostrum itaque non non adipisci. Ut delectus quis delectus est at sint. Iste hic qui ea eaque eaque sed id. Hic placeat rerum numquam id velit deleniti voluptatem. Illum adipisci voluptas adipisci ut alias. Earum exercitationem iste quidem eveniet aliquid hic reiciendis. Exercitationem est sunt in minima consequuntur. Aut quaerat libero dolorem.</p>",
+      MR: [
+        {
+          MR1: 1,
+          MR2: 1,
+          MR3: [1, false],
+        },
+      ],
+      TP: [
+        {
+          TP1: 1,
+          TP2: 2,
+        },
+      ],
+      CSF: [
+        {
+          CSF1: 1,
+        },
+      ],
+      views: 143,
+      average_note: 2.72198,
+      undocumented: true,
+      published_at: new Date("2012-08-06"),
+      tags: [1, 3],
+      category: "tech",
+      subcategory: "computers",
+      backlinks: [
+        {
+          date: "2012-08-09T00:00:00.000Z",
+          url: "http://example.com/bar/baz.html",
+        },
+      ],
+      notifications: [12, 31, 42],
+    },
+  ],
+  comments: [
+    {
+      id: 1,
+      author: {},
+      post_id: 6,
+      body:
+        "Queen, tossing her head through the wood. 'If it had lost something; and she felt sure it.",
+      created_at: new Date("2012-08-02"),
+    },
+    {
+      id: 2,
+      author: {
+        name: "Kiley Pouros",
+        email: "kiley@gmail.com",
+      },
+      post_id: 9,
+      body:
+        "White Rabbit: it was indeed: she was out of the ground--and I should frighten them out of its right paw round, 'lives a March Hare. 'Sixteenth,'.",
+      created_at: new Date("2012-08-08"),
+    },
+    {
+      id: 3,
+      author: {
+        name: "Justina Hegmann",
+      },
+      post_id: 3,
+      body:
+        "I'm not Ada,' she said, 'and see whether it's marked \"poison\" or.",
+      created_at: new Date("2012-08-02"),
+    },
+    {
+      id: 4,
+      author: {
+        name: "Ms. Brionna Smitham MD",
+      },
+      post_id: 6,
+      body:
+        "Dormouse. 'Fourteenth of March, I think I can say.' This was such a noise inside, no one else seemed inclined.",
+      created_at: new Date("2014-09-24"),
+    },
+    {
+      id: 5,
+      author: {
+        name: "Edmond Schulist",
+      },
+      post_id: 1,
+      body:
+        "I ought to tell me your history, you know,' the Hatter and the happy summer days. THE.",
+      created_at: new Date("2012-08-07"),
+    },
+    {
+      id: 6,
+      author: {
+        name: "Danny Greenholt",
+      },
+      post_id: 6,
+      body:
+        "Duchess asked, with another hedgehog, which seemed to be lost: away went Alice after it, never once considering how in the other. In the very tones of.",
+      created_at: new Date("2012-08-09"),
+    },
+    {
+      id: 7,
+      author: {
+        name: "Luciano Berge",
+      },
+      post_id: 5,
+      body:
+        "While the Panther were sharing a pie--' [later editions continued as follows.",
+      created_at: new Date("2012-09-06"),
+    },
+    {
+      id: 8,
+      author: {
+        name: "Annamarie Mayer",
+      },
+      post_id: 5,
+      body:
+        "I tell you, you coward!' and at once and put it more clearly,' Alice.",
+      created_at: new Date("2012-10-03"),
+    },
+    {
+      id: 9,
+      author: {
+        name: "Breanna Gibson",
+      },
+      post_id: 2,
+      body:
+        "THAT. Then again--\"BEFORE SHE HAD THIS FIT--\" you never tasted an egg!' 'I HAVE tasted eggs, certainly,' said Alice, as she spoke. Alice did not like to have it.",
+      created_at: new Date("2012-11-06"),
+    },
+    {
+      id: 10,
+      author: {
+        name: "Logan Schowalter",
+      },
+      post_id: 3,
+      body:
+        "I'd been the whiting,' said the Hatter, it woke up again with a T!' said the Gryphon. '--you advance twice--' 'Each with a growl, And concluded the banquet--] 'What IS the fun?' said.",
+      created_at: new Date("2012-12-07"),
+    },
+    {
+      id: 11,
+      author: {
+        name: "Logan Schowalter",
+      },
+      post_id: 1,
+      body:
+        "I don't want to be?' it asked. 'Oh, I'm not Ada,' she said, 'and see whether it's marked \"poison\" or not'; for she had asked it aloud; and in despair she put her hand on the end of the.",
+      created_at: new Date("2012-08-05"),
+    },
+  ],
+  tags: [
+    {
+      id: 1,
+      name: "Sport",
+      published: 1,
+    },
+    {
+      id: 2,
+      name: "Technology",
+      published: false,
+    },
+    {
+      id: 3,
+      name: "Code",
+      published: true,
+    },
+    {
+      id: 4,
+      name: "Photo",
+      published: false,
+    },
+    {
+      id: 5,
+      name: "Music",
+      published: 1,
+    },
+    {
+      id: 6,
+      name: "Parkour",
+      published: 1,
+      parent_id: 1,
+    },
+    {
+      id: 7,
+      name: "Crossfit",
+      published: 1,
+      parent_id: 1,
+    },
+    {
+      id: 8,
+      name: "Computing",
+      published: 1,
+      parent_id: 2,
+    },
+    {
+      id: 9,
+      name: "Nanoscience",
+      published: 1,
+      parent_id: 2,
+    },
+    {
+      id: 10,
+      name: "Blockchain",
+      published: 1,
+      parent_id: 2,
+    },
+    {
+      id: 11,
+      name: "Node",
+      published: 1,
+      parent_id: 3,
+    },
+    {
+      id: 12,
+      name: "React",
+      published: 1,
+      parent_id: 3,
+    },
+    {
+      id: 13,
+      name: "Nature",
+      published: 1,
+      parent_id: 4,
+    },
+    {
+      id: 14,
+      name: "People",
+      published: 1,
+      parent_id: 4,
+    },
+    {
+      id: 15,
+      name: "Animals",
+      published: 1,
+      parent_id: 13,
+    },
+    {
+      id: 16,
+      name: "Moutains",
+      published: 1,
+      parent_id: 13,
+    },
+    {
+      id: 17,
+      name: "Rap",
+      published: 1,
+      parent_id: 5,
+    },
+    {
+      id: 18,
+      name: "Rock",
+      published: 1,
+      parent_id: 5,
+    },
+    {
+      id: 19,
+      name: "World",
+      published: 1,
+      parent_id: 5,
+    },
+  ],
+  users: [
+    {
+      id: 1,
+      name: "Logan Schowalter",
+      role: "admin",
+    },
+    {
+      id: 2,
+      name: "Breanna Gibson",
+      role: "user",
+    },
+    {
+      id: 3,
+      name: "Annamarie Mayer",
+      role: "user",
+    },
+  ],
+};
---
+++ src/posts/PostCreate.js
@@ -0,0 +1,151 @@
+import * as React from "react";
+import { useMemo } from "react";
+import RichTextInput from "ra-input-rich-text";
+import {
+  ArrayInput,
+  AutocompleteInput,
+  BooleanInput,
+  Create,
+  DateInput,
+  FormDataConsumer,
+  RadioButtonGroupInput,
+  ReferenceInput,
+  SaveButton,
+  SelectInput,
+  SimpleForm,
+  SimpleFormIterator,
+  TextInput,
+  Toolbar,
+  required,
+} from "react-admin"; // eslint-disable-line import/no-unresolved
+import { FormSpy } from "react-final-form";
+
+const PostCreateToolbar = (props) => (
+  <Toolbar {...props}>
+    <SaveButton
+      label="post.action.save_and_edit"
+      redirect="edit"
+      submitOnEnter={true}
+    />
+    <SaveButton
+      label="post.action.save_and_show"
+      redirect="show"
+      submitOnEnter={false}
+      variant="text"
+    />
+    <SaveButton
+      label="post.action.save_and_add"
+      redirect={false}
+      submitOnEnter={false}
+      variant="text"
+    />
+    <SaveButton
+      label="post.action.save_with_average_note"
+      transform={(data) => ({ ...data, average_note: 10 })}
+      redirect="show"
+      submitOnEnter={false}
+      variant="text"
+    />
+  </Toolbar>
+);
+
+const backlinksDefaultValue = [
+  {
+    date: new Date(),
+    url: "http://google.com",
+  },
+];
+const PostCreate = ({ permissions, ...props }) => {
+  const initialValues = useMemo(
+    () => ({
+      average_note: 0,
+    }),
+    []
+  );
+
+  const dateDefaultValue = useMemo(() => new Date(), []);
+
+  return (
+    <Create {...props}>
+      <SimpleForm
+        toolbar={<PostCreateToolbar />}
+        initialValues={initialValues}
+        validate={(values) => {
+          const errors = {};
+          ["ticket"].forEach((field) => {
+            if (!values[field]) {
+              errors[field] = "Required field";
+            }
+          });
+
+          return errors;
+        }}
+      >
+        <TextInput autoFocus source="ticket" />
+        <RadioButtonGroupInput
+          source="message relevance"
+          choices={[
+            { id: "yes", name: "Yes" },
+            { id: "no", name: "No" },
+            { id: "na", name: "N/A" },
+          ]}
+        />
+        <BooleanInput source="undocumented" defaultValue />
+        <RichTextInput source="note" validate={required()} />
+        {/* <FormSpy subscription={{ values: true }}>
+          {({ values }) =>
+            values.ticket ? <NumberInput source="average_note" /> : null
+          }
+        </FormSpy> */}
+
+        <DateInput source="published_at" defaultValue={dateDefaultValue} />
+        <ArrayInput
+          source="backlinks"
+          defaultValue={backlinksDefaultValue}
+          validate={[required()]}
+        >
+          <SimpleFormIterator>
+            <DateInput source="date" />
+            <TextInput source="url" />
+          </SimpleFormIterator>
+        </ArrayInput>
+        {permissions === "admin" && (
+          <ArrayInput source="authors">
+            <SimpleFormIterator>
+              <ReferenceInput label="User" source="user_id" reference="users">
+                <AutocompleteInput />
+              </ReferenceInput>
+              <FormDataConsumer>
+                {({ formData, scopedFormData, getSource, ...rest }) =>
+                  scopedFormData && scopedFormData.user_id ? (
+                    <SelectInput
+                      label="Role"
+                      source={getSource("role")}
+                      choices={[
+                        {
+                          id: "headwriter",
+                          name: "Head Writer",
+                        },
+                        {
+                          id: "proofreader",
+                          name: "Proof reader",
+                        },
+                        {
+                          id: "cowriter",
+                          name: "Co-Writer",
+                        },
+                      ]}
+                      {...rest}
+                    />
+                  ) : null
+                }
+              </FormDataConsumer>
+            </SimpleFormIterator>
+          </ArrayInput>
+        )}
+      </SimpleForm>
+    </Create>
+  );
+};
+
+export default PostCreate;
